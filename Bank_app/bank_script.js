c9fe13f5ee9fd11e7163c5e164dbce15fbb46d5e
Ather06 <mdatherbiswas06@gmail.com>
2024-04-15 21:42:23 +0600

    bank app project

---
+++ Bank_app/bank_script.js
@@ -0,0 +1,35 @@
+// bank app scripts
+console.log('Heloow')
+class BankAccount{
+
+    //declare account number,accountowner name and balance
+    constructor(accountNumber, accountOwnerName, balance){
+        this.accountNumber = accountNumber;
+        this.accountOwnerName = accountOwnerName;
+        this.balance = balance;
+    }
+
+    //methods for deposit amount
+    deposit(amount){
+        this.balance += amount;
+    }
+
+    //methods for withdrawn amount
+    withdrawn(amount){
+        if(amount < this.balance){
+            this.balance -= amount;
+        }
+        else {
+            console.log(`Insufficient balance in this account ${this.accountNumber}`)
+        }
+    }
+    
+    //method for display account information
+    displayAccountInfo(){
+        console.log(`Account ${this.accountNumber} is the owner of ${this.accountOwnerName} and This account balance: ${this.balance}`)
+    }
+}
+const account = new BankAccount('001', 'Jhon Doe', 2000);
+account.deposit(500);
+account.withdrawn(1000);
+account.displayAccountInfo();
