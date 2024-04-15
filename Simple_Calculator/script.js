9f9ebdee578dfedd4ff963700794260c5253a499
Ather06 <mdatherbiswas06@gmail.com>
2024-04-15 20:56:28 +0600

    Revised project

--- Simple_Calculator/script.js
+++ Simple_Calculator/script.js
@@ -1,15 +1,20 @@
-function calc(){
+
+    
+function calc(event){
+    event.preventDefault();
     let number1 = parseFloat(document.getElementById('number1').value);
     let number2 = parseFloat(document.getElementById('number2').value);
     let operators = document.getElementById('operators').value;
-
+    // let result = document.getElementById("result").value;
+    
     if(operators == "+")
     {
-        document.getElementById("result").value = number1 + number2;
+        document.getElementById("result").value=number1 + number2; 
+        
     }
     if(operators == "-")
     {
-        document.getElementById("result").value = number1 - number2;
+        document.getElementById("result").value= number1 - number2;
     }
     if(operators == "X")
     {
